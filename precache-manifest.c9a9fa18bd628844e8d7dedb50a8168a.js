db10e69427314e0ec08b8088afdf97055e664fe5
Michael Campbell <michael.campbell14@gmail.com>
2020-03-11 11:21:53 -0400

    Updates

---
+++ precache-manifest.c9a9fa18bd628844e8d7dedb50a8168a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1afa3d610eff02508e2defbade06b3",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0920fc1c34b6a4f4c3df",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "015d75d7529a40ee504d",
+    "url": "/react-tutorial/static/js/2.22b8f73d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tutorial/static/js/2.22b8f73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0920fc1c34b6a4f4c3df",
+    "url": "/react-tutorial/static/js/main.dc57e934.chunk.js"
+  },
+  {
+    "revision": "1b20ea05cd43bdedcf8b",
+    "url": "/react-tutorial/static/js/runtime-main.59791939.js"
+  }
+]);
